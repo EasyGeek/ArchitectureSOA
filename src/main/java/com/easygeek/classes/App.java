f4cebbacda38d857338b5bfdc3431d83a3d228c4
Nicolas Devynck <nicolas.devynck@epsi.fr>
2015-04-03 12:17:23 +0200

    Add hibernate configuration

---
+++ src/main/java/com/easygeek/classes/App.java
@@ -0,0 +1,30 @@
+package com.easygeek.classes;
+
+
+import java.util.List;
+
+import javax.persistence.Entity;
+
+import org.hibernate.Session;
+
+/**
+ * Hello world!
+ *
+ */
+@Entity
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Session session = HibernateUtil.getSessionFactory()
+        							   .openSession();
+        session.beginTransaction();
+		List<Client> result = (List<Client>) session.createQuery("from Client").list();
+        session.getTransaction().commit();
+       
+        for(Client f : (List<Client>)result)
+        {
+        	System.out.println(f.getNom() + " " + f.getPrenom());
+        }     
+    }
+}
