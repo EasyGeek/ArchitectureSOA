368b286bf63916b09de86a3fe90e4b2d356fd8e9
rmaurice <remy.maurice@epsi.fr>
2015-04-13 12:01:53 +0200

    catalogue

--- src/main/java/com/easygeek/controller/CatalogueController.java
+++ src/main/java/com/easygeek/controller/CatalogueController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.easygeek.entite.Composant;
+import com.easygeek.entite.Marque;
 import com.easygeek.entite.Stock;
+import com.easygeek.entite.Type;
 import com.easygeek.service.CatalogueService;
 import com.easygeek.service.StockService;
 
@@ -26,15 +28,29 @@ public class CatalogueController {
 		return composants;
 	}
 	
+	@RequestMapping(value = "/type",  method = RequestMethod.GET)
+	public List<Type> getAllType() {
+		List<Type> types = catalogueService.getAllType();
+		return types;
+	}
+	
 	@RequestMapping(value = "/type/{id}",  method = RequestMethod.GET)
 	public List<Composant> getAllComposantsByType(@PathVariable  Integer id) {
 		List<Composant> composants = catalogueService.getAllComposantByType(id);
 		return composants;
 	}
 	
+	@RequestMapping(value = "/marque",  method = RequestMethod.GET)
+	public List<Marque> getAllMarque() {
+		List<Marque> marques = catalogueService.getAllMarque();
+		return marques;
+	}	
+	
 	@RequestMapping(value = "/marque/{id}",  method = RequestMethod.GET)
 	public List<Composant> getAllComposantsByMarque(@PathVariable Integer id) {
-		List<Composant> composants = catalogueService.getAllComposantByType(id);
+		List<Composant> composants = catalogueService.getAllComposantByMarque(id);
 		return composants;
 	}	
+	
+	
 }
--- src/main/java/com/easygeek/service/CatalogueService.java
+++ src/main/java/com/easygeek/service/CatalogueService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.easygeek.dao.CoreDao;
 import com.easygeek.entite.Client;
 import com.easygeek.entite.Composant;
+import com.easygeek.entite.Marque;
+import com.easygeek.entite.Type;
 
 @Service
 public class CatalogueService extends CoreDao<Composant>{
@@ -25,5 +27,15 @@ public class CatalogueService extends CoreDao<Composant>{
 	@SuppressWarnings("unchecked")
 	public List<Composant> getAllComposantByType(Integer idType){
 		return session.createCriteria(Composant.class).add(Restrictions.eq("typeId",idType)).list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Marque> getAllMarque(){
+		return session.createCriteria(Marque.class).list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Type> getAllType(){
+		return session.createCriteria(Type.class).list();
 	}
 }
