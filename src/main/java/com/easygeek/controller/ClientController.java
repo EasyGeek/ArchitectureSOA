f9c372736b8e7468ccf6c8c544dbd5f7f12a8e19
quentinlogie <quentin.logie@epsi.fr>
2015-04-14 09:58:08 +0200

    change post/delete methode for utilisateur and fournisseur

--- src/main/java/com/easygeek/controller/ClientController.java
+++ src/main/java/com/easygeek/controller/ClientController.java
@@ -1,18 +1,13 @@
 package com.easygeek.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
 import com.easygeek.entite.Client;
 import com.easygeek.service.ClientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/client")
@@ -34,142 +29,61 @@ public class ClientController {
 		return client;
 	}
 	
-	@RequestMapping(value = "/connexion", method = RequestMethod.POST)
-	public boolean connexion(@RequestBody String c) throws UnsupportedEncodingException{
-		String email = null, password = null;
-		String message = "";
-		String champs[] = c.split("&");
-		Client client = new Client();
-		for (int i = 0; i < champs.length; i++){
-			String array[] = champs[i].split("=");
-			switch(array[0]) {
-			case "email":
-				email = array[1];
-				break;
-			case "password":
-				password = array[1];
-				break;
-			}
-		}
-		return clientService.connexion(email, password);
-	}
+//	@RequestMapping(value = "/connexion", method = RequestMethod.POST)
+//	public boolean connexion(@RequestBody String c) throws UnsupportedEncodingException{
+//		String email = null, password = null;
+//		String message = "";
+//		String champs[] = c.split("&");
+//		Client client = new Client();
+//		for (int i = 0; i < champs.length; i++){
+//			String array[] = champs[i].split("=");
+//			switch(array[0]) {
+//			case "email":
+//				email = array[1];
+//				break;
+//			case "password":
+//				password = array[1];
+//				break;
+//			}
+//		}
+//		return clientService.connexion(email, password);
+//	}
 	
 	@RequestMapping(value = "/ajouter", method = RequestMethod.POST)
-	public String ajoutClient(@RequestBody String c, @PathVariable Integer id){
-		String message = "";
-		String champs[] = c.split("&");
-		Client client = new Client();
-		for (int i = 0; i < champs.length; i++){
-			String array[] = champs[i].split("=");
-			switch(array[0]) {
-				case "nom":
-					client.setNom(array[1]);
-					break;
-				case "prenom":
-					client.setNom(array[1]);
-					break;
-				case "adresse":
-					client.setAdresse(array[1]);
-					break;
-				case "codePostal":
-					client.setCodePostal(array[1]);
-					break;
-				case "ville":
-					client.setVille(array[1]);
-					break;
-				case "telephone":
-					client.setTelephone(array[1]);
-					break;
-				case "email":
-					client.setEmail(array[1]);
-					break;
-				case "password":
-					client.setPassword(array[1]);
-					break;
-			}   
-	    }
-
+	public ResponseEntity ajoutClient(@RequestBody Client c){
 		try {
-			clientService.save(client);
-			message = "Client ajouté avec succès =)";
-			System.out.println("Ajout d'un fournisseur avec l'id :" + client.getClientId());
+			clientService.save(c);
+			System.out.println("Ajout d'un fournisseur avec l'id :" + c.getClientId());
 		} catch (Exception e) {
-			message = "Problème lors de l'ajout du client";
 			System.out.println("Erreur lors de l'ajout du client");
 		}
-		return message;
+		return new ResponseEntity<Client>(c, HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/modifier/{id}",  method = { RequestMethod.GET, RequestMethod.POST })
-	public String modifierClient(@RequestBody String c, @PathVariable Integer id) throws UnsupportedEncodingException{
-		String message = "";
-		Client client = clientService.get(id);
-		
-		String champs[] = c.split("&");
-		for (int i = 0; i < champs.length; i++){
-			String array[] = champs[i].split("=");
-			switch(array[0]) {
-			case "nom":
-				client.setNom(array[1]);
-				break;
-			case "prenom":
-				client.setNom(array[1]);
-				break;
-			case "adresse":
-				client.setAdresse(array[1]);
-				break;
-			case "codePostal":
-				client.setCodePostal(array[1]);
-				break;
-			case "ville":
-				client.setVille(array[1]);
-				break;
-			case "telephone":
-				client.setTelephone(array[1]);
-				break;
-			case "email":
-				array[1] = new String(array[1].getBytes("iso-8859-1"), "utf8");
-				client.setEmail(array[1]);
-				break;
-			case "password":
-				client.setPassword(array[1]);
-				break;
-			}   
-	    }
+	@RequestMapping(value = "/modifier", method = RequestMethod.POST)
+	public ResponseEntity modifierClient(@RequestBody Client c) {
 		try {
-			clientService.update(client);
-			message = "Client modifié.";
-			System.out.println("Modification du client avec l'id :" + client.getClientId());
+			clientService.update(c);
+			System.out.println("Modification du client avec l'id :" + c.getClientId());
 		} catch (Exception e) {
-			message = "Problème lors de la modification du client";
 			System.out.println("Erreur lors de la modification du client");
 		}
-		return message;
+		return new ResponseEntity<Client>(c, HttpStatus.OK);
 	}
 	
 	/*** Suppression d'un client en envoyant en DELETE le champ id 
 	   http://localhost:8080/fournisseur/supprimer ***/
 	@RequestMapping(value = "/supprimer",  method = RequestMethod.DELETE)
-	public String supprimerClient(@RequestBody String f) {
-		String message = "";
-		if(!f.contains("&")) {
-			String idValue[] = f.split("=");
-			int id = Integer.parseInt(idValue[1]);
-			
-			Client client = clientService.get(id);
-			
-			try {
-				clientService.delete(client);
-				message = "Client supprimer";
-				System.out.println(message);
-			} catch (Exception e) {
-				message = "Problème lors de la suppression du client";
-				System.out.println(message);
-			}
-		} else {
-			message = "bad argument";
+	public String supprimerClient(@RequestBody Client c) {
+		String message;
+		try {
+			clientService.delete(c);
+			message = "Success";
+			System.out.println("Client supprimer");
+		} catch (Exception e) {
+			message = "Failed";
+			System.out.println("Problème lors de la suppression du client");
 		}
-		
 		return message;
 	}
 }
--- src/main/java/com/easygeek/controller/FournisseurController.java
+++ src/main/java/com/easygeek/controller/FournisseurController.java
@@ -2,6 +2,8 @@ package com.easygeek.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,111 +36,45 @@ public class FournisseurController {
 		return fournisseur;
 	}
 	
-	/*** Ajoute un fournisseur en passant en POST les champs à renseigner
+	/*** Ajoute un fournisseur en passant un json contenant l'objet en POST
 	 * http://localhost:8080/fournisseur/ajouter ***/
 	@RequestMapping(value = "/ajouter", method = RequestMethod.POST)
-	public String ajoutFournisseur(@RequestBody String f) {
-		String message = "";
-		String champs[] = f.split("&");
-		Fournisseur fournisseur = new Fournisseur();
-		for (int i = 0; i < champs.length; i++){
-			String array[] = champs[i].split("=");
-			switch(array[0]) {
-				case "nom":
-					fournisseur.setNom(array[1]);
-					break;
-				case "telephone":
-					fournisseur.setTelephone(array[1]);
-					break;
-				case "adresse":
-					fournisseur.setAdresse(array[1]);
-					break;
-				case "codePostal":
-					fournisseur.setCodePostal(array[1]);
-					break;
-				case "ville":
-					fournisseur.setVille(array[1]);
-					break;
-			}   
-	    }
-
+	public ResponseEntity ajoutFournisseur(@RequestBody Fournisseur c){
 		try {
-			fournisseurService.save(fournisseur);
-			message = "Fournisseur ajouter =)";
-			System.out.println("Ajout d'un fournisseur avec l'id :" + fournisseur.getFournisseurId());
+			fournisseurService.save(c);
+			System.out.println("Ajout d'un fournisseur avec l'id :" + c.getFournisseurId());
 		} catch (Exception e) {
-			message = "Problème lors de l'ajout d'un fournisseur";
-			System.out.println(message);
+			System.out.println("Erreur lors de l'ajout du client");
 		}
-
-		return message;
+		return new ResponseEntity<Fournisseur>(c, HttpStatus.OK);
 	}
 	
 	/*** Modifie un fournisseur en envoyant en post les champs à modifier et en get l'id de l'utilisateur 
 	 * http://localhost:8080/fournisseur/modifier/{id} ***/
-	@RequestMapping(value = "/modifier/{id}",  method = { RequestMethod.GET, RequestMethod.POST })
-	public String modifierFournisseur(@RequestBody String f, @PathVariable Integer id) {
-		String message = "";
-		Fournisseur fournisseur = fournisseurService.get(id);
-		
-		
-		String champs[] = f.split("&");
-		for (int i = 0; i < champs.length; i++){
-			String array[] = champs[i].split("=");
-			switch(array[0]) {
-				case "nom":
-					fournisseur.setNom(array[1]);
-					break;
-				case "telephone":
-					fournisseur.setTelephone(array[1]);
-					break;
-				case "adresse":
-					fournisseur.setAdresse(array[1]);
-					break;
-				case "codePostal":
-					fournisseur.setCodePostal(array[1]);
-					break;
-				case "ville":
-					fournisseur.setVille(array[1]);
-					break;
-			}   
-	    }
-		
+	@RequestMapping(value = "/modifier", method = RequestMethod.POST)
+	public ResponseEntity modifierFournisseur(@RequestBody Fournisseur c) {
 		try {
-			fournisseurService.update(fournisseur);
-			message = "Modification du fournisseur avec l'id :" + fournisseur.getFournisseurId();
-			System.out.println(message);
+			fournisseurService.update(c);
+			System.out.println("Modification du client avec l'id :" + c.getFournisseurId());
 		} catch (Exception e) {
-			message = "Problème lors de la modification d'un fournisseur";
-			System.out.println(message);
+			System.out.println("Erreur lors de la modification du client");
 		}
-		
-		return message;
+		return new ResponseEntity<Fournisseur>(c, HttpStatus.OK);
 	}
 	
 	/*** Suppression d'un fournisseur en envoyant en DELETE le champ id 
 	   http://localhost:8080/fournisseur/supprimer ***/
 	@RequestMapping(value = "/supprimer",  method = RequestMethod.DELETE)
-	public String supprimerFournisseur(@RequestBody String f) {
-		String message = "";
-		if(!f.contains("&")) {
-			String idValue[] = f.split("=");
-			int id = Integer.parseInt(idValue[1]);
-			
-			Fournisseur fournisseur = fournisseurService.get(id);
-			
-			try {
-				fournisseurService.delete(fournisseur);
-				message = "Fournisseur supprimer";
-				System.out.println(message);
-			} catch (Exception e) {
-				message = "Problème lors de la suppression d'un fournisseur";
-				System.out.println(message);
-			}
-		} else {
-			message = "bad argument";
+	public String supprimerFournisseur(@RequestBody Fournisseur c) {
+		String message;
+		try {
+			fournisseurService.delete(c);
+			message = "Success";
+			System.out.println("Fournisseur supprimer");
+		} catch (Exception e) {
+			message = "Failed";
+			System.out.println("Problème lors de la suppression du fournisseur");
 		}
-		
 		return message;
 	}
 }
