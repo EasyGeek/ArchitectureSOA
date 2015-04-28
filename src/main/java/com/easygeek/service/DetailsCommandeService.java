875abe493ef303a6777161b81c75142b8a81d77a
quentinPr <quentin.pruvost0@gmail.com>
2015-04-29 00:18:51 +0200

    DetailsCommande ajout/suppression/modification

--- src/main/java/com/easygeek/service/DetailsCommandeService.java
+++ src/main/java/com/easygeek/service/DetailsCommandeService.java
@@ -1,9 +1,12 @@
 package com.easygeek.service;
 
+import org.hibernate.Criteria;
 import org.hibernate.FetchMode;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
 import com.easygeek.dao.CoreDao;
+import com.easygeek.entite.Composant;
 import com.easygeek.entite.DetailsCommande;
 
 public class DetailsCommandeService extends CoreDao<DetailsCommande> {
@@ -11,15 +14,22 @@ public class DetailsCommandeService extends CoreDao<DetailsCommande> {
 	public DetailsCommandeService() {
 		super(DetailsCommande.class);
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	public DetailsCommande getDetails(Integer idCommande){
-		
-	return (DetailsCommande) session.createCriteria(DetailsCommande.class).add(Restrictions.eq("commandeId", idCommande))
-			.setFetchMode("details_commande", FetchMode.JOIN)
-			.uniqueResult();
+		return (DetailsCommande) session.createCriteria(DetailsCommande.class).add(Restrictions.eq("id.commandeId",idCommande)).uniqueResult();
+	}
 	
-	} 
-
-
+	public Composant getCommandeDetailsComposants(Integer idCommande){
+	/*	Criteria criteria = session.createCriteria(DetailsCommande.class).add(Restrictions.eq("id.commandeId", idCommande));
+		criteria.setFetchMode("Commande",FetchMode.JOIN);
+		criteria.add(Restrictions.eq("commandeId", idCommande));
+		
+		return (DetailsCommande) criteria.uniqueResult();*/
+		
+		DetailsCommande Detail = (DetailsCommande) session.createCriteria(DetailsCommande.class).add(Restrictions.eq("id.commandeId",idCommande)).uniqueResult();
+		
+		return (Composant) session.createCriteria(Composant.class).add(Restrictions.eq("reference",Detail.getId().getReference())).uniqueResult();
+		
+	}
+	
 }
