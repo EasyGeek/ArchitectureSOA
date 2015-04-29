f2e2151c125f77f3e63a0b84f5a24d5e31b71ccf
quentinlogie <quentin.logie@epsi.fr>
2015-04-29 15:24:28 +0200

    Add service composantfournisseur

--- src/main/java/com/easygeek/service/ComposantFournisseurService.java
+++ src/main/java/com/easygeek/service/ComposantFournisseurService.java
@@ -1,10 +1,20 @@
 package com.easygeek.service;
 
+import java.util.List;
+
+import org.hibernate.criterion.Restrictions;
+
 import com.easygeek.dao.CoreDao;
 import com.easygeek.entite.ComposantFournisseur;
 
 public class ComposantFournisseurService extends CoreDao<ComposantFournisseur> {
+
     public ComposantFournisseurService() {
         super(ComposantFournisseur.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<ComposantFournisseur> getDetails(Integer idFournisseur){
+        return (List<ComposantFournisseur>) session.createCriteria(ComposantFournisseur.class).add(Restrictions.eq("fournisseur.fournisseurId", idFournisseur)).list();
     }
 }
